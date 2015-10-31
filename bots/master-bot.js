2ec1a42cea7cde259e01147fcca3be2bb751f4f0
Daniel Rasmuson <dan123911@gmail.com>
2015-10-31 14:28:24 -0700

    cleaned up getting bot just a bit

--- bots/master-bot.js
+++ bots/master-bot.js
@@ -22,38 +22,47 @@ function help(botName, allCommands){
   return '*Please send one of the possible commands.*\n\n'+possibleCommands;
 }
 
+function getBot(botName){
+  return Rx.Observable.create(function(observer){
+    // todo have this loop
+    var bot = _.first(bots.filter(function(bot){
+      return botCMDName(bot.name) === botName;
+    }));
+
+    if (bot === undefined){
+      observer.onError('Bot not found: '+botName+'. Known bots: '+bots.map(function(bot){
+        return botCMDName(bot.name);
+      }).join(', '));
+    } else{
+      observer.onNext(bot);
+      observer.onCompleted();
+    }
+  });
+}
+
 module.exports = {
   ask: function(question){
     return Rx.Observable.create(function(observer){
 
       var commandWords = question.split(' ');
 
-      // todo have this loop
-      var bot = _.first(bots.filter(function(bot){
-        return botCMDName(bot.name) === commandWords[0];
-      }));
+      getBot(commandWords[0]).forEach(function(bot){
+        var command = bot.api[commandWords[1]];
+        var commandBody = commandWords.slice(2).join(' ');
 
-      if (bot === undefined){
-        observer.onError('Bot not found: '+commandWords[0]+'. Known bots: '+bots.map(function(bot){
-          return botCMDName(bot.name);
-        }).join(', '));
-        return;
-      }
-
-      var command = bot.api[commandWords[1]];
-      var commandBody = commandWords.slice(2).join(' ');
-
-      if (command){
-        command(commandBody).forEach(function(commandResponse){
-          observer.onNext(answer(bot.name, bot.icon, commandResponse));
+        if (command){
+          command(commandBody).forEach(function(commandResponse){
+            observer.onNext(answer(bot.name, bot.icon, commandResponse));
+            observer.onCompleted();
+          });
+        } else{
+          var helpMessage = help(bot.name, _.keys(bot.api));
+          observer.onNext(answer(bot.name, bot.icon, helpMessage));
           observer.onCompleted();
-        });
-      } else{
-        var helpMessage = help(bot.name, _.keys(bot.api));
-        observer.onNext(answer(bot.name, bot.icon, helpMessage));
-        observer.onCompleted();
-      }
-
+        }
+      }, function(err){
+        observer.onError(err);
+      })
     });
   }
 }
