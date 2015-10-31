b1003de3c6e595e4f3c17b43916cc0ba2fe5e053
Daniel Rasmuson <dan123911@gmail.com>
2015-10-31 14:08:27 -0700

    minior changes

--- bots/goals/goal-bot.js
+++ bots/goals/goal-bot.js
@@ -8,10 +8,13 @@ var db = low('db.json')
 var goalBotApi = {
   list: function(){
     return Rx.Observable.create(function(observer){
-        var goalNames = db.object.goals.map(function(goal, index){
-          return index+1+". "+goal.title;
-        }).join('\n');
-       observer.onNext(goalNames);
+      if (db.object.goals === undefined){
+        observer.onNext('No Current Goals');
+      };
+      var goalNames = db.object.goals.map(function(goal, index){
+        return index+1+". "+goal.title;
+      }).join('\n');
+      observer.onNext(goalNames);
     });
   },
   new: function(text){
